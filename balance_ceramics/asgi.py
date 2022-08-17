679d99685a317a0853f6b761e81439cd935494ff
JJohnyy <jan.jasa@email.cz>
2022-08-17 12:53:21 +0000

    started project

---
+++ balance_ceramics/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for balance_ceramics project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'balance_ceramics.settings')
+
+application = get_asgi_application()
