059a3e299e8f720725ca3c5481b6a2521a3a9d86
JJohnyy <jan.jasa@email.cz>
2022-08-18 13:33:36 +0000

    checkout admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Order
 # Register your models here.
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """ Settings for orders in admin """
+    list_display = (
+        'date',
+        'order_number',
+        'full_name',
+        'email',
+        'phone_number',
+    )
