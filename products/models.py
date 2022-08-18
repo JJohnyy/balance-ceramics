9126f0b214810bd942682ad70d13343782fb5a63
JJohnyy <jan.jasa@email.cz>
2022-08-18 07:28:30 +0000

    products views, models, html

--- products/models.py
+++ products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class MugsCategory(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Mugs(models.Model):
+    category = models.ForeignKey('MugsCategory', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    image = models.ImageField(null = True, blank = True)
+    slug_name = models.CharField(max_length=255, unique = True)
+    price = models.DecimalField(max_digits = 6, decimal_places = 2)
+
+    def __str__(self):
+        return self.name
