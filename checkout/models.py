7b21aab2785ed4417e8a8f8f9889ff09ac111978
JJohnyy <jan.jasa@email.cz>
2022-08-18 13:31:45 +0000

    created checkout models

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,129 @@
+import uuid
+
 from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from products.models import Dishes, Wines, Bundle
+from profiles.models import UserProfile
 
 # Create your models here.
+
+class Order(models.Model):
+    """ Model for an order """
+
+    order_number = models.CharField(
+        max_length=32,
+        null=False,
+        editable=False
+        )
+    user_profile = models.ForeignKey(
+        UserProfile,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='orders'
+        )
+    full_name = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False
+        )
+    email = models.EmailField(
+        max_length=254,
+        null=False,
+        blank=False
+        )
+    phone_number = models.CharField(
+        max_length=20,
+        null=False,
+        blank=False
+        )
+    date = models.DateTimeField(
+        auto_now_add=True
+        )
+    order_total = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=False,
+        default=0
+        )
+    grand_total = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=False,
+        default=0
+        )
+    original_bag = models.TextField(
+        null=False,
+        blank=False,
+        default=''
+        )
+    stripe_pid = models.CharField(
+        max_length=254,
+        null=False,
+        blank=False,
+        default=''
+        )
+
+    def __str__(self):
+        return f'{self.order_number}'
+
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """
+        Update grand total each time a order item is added
+        """
+        self.order_total = self.orderitems.aggregate(
+            Sum('orderitem_total'))['orderitem_total__sum'] or 0
+
+        self.grand_total = self.order_total
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+
+class OrderItem(models.Model):
+    """ Model for individual order item """
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        related_name='orderitems'
+    )
+    mug = models.ForeignKey(
+        Mugs,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+
+    def save(self, *args, **kwargs):
+         """
+        Override the original save method to set the orderitem total
+        and update the order total depending on if the.
+        """
+        if self.mug:
+            self.order_total = self.mug.price * self.quantity
+            super().save(*args, **kwargs)
+
+        else:
+            return
+
+    def __str__(self):
+        return f'{self.order}'
+
+
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,174 @@
-from django.shortcuts import render
+import stripe
+import json
+
+from django.views.decorators.http import require_POST
+from django.shortcuts import (render, redirect, reverse,
+                              get_object_or_404, HttpResponse)
+from django.contrib import messages
+from django.conf import settings
+
+from products.models import Mugs
+from bag.contexts import bag_contents
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
+from .forms import Order, OrderForm
+from .models import OrderItem
 
 # Create your views here.
+
+def checkout(request):
+    """
+    Checkout page view with order form it there are items in the
+    session and a error message if the bag is empty and the url
+    is forced
+    """
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+    if request.method == 'POST':
+        bag = request.session.get('bag', {})
+
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+        }
+        order_form = OrderForm(form_data)
+
+        if order_form.is_valid():
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+
+            order.original_bag = json.dumps(bag)
+            order_form.save()
+
+            mugs = Mugs.objects.all()
+
+            mugs_slug_list = []
+
+            for mug in mugs:
+                mugs_slug_list.append(mug.slug_name)
+
+            for item_slug, item_data in bag.items():
+                try:
+                    if item_slug in mugs_slug_list:
+                        mug = Mugs.objects.get(slug=item_slug)
+                        order_item = OrderItem(
+                            order=order,
+                            mug = mug,
+                            quantity=item_data,
+                        )
+                        order_item.save()
+                except:
+                        Mugs.DoesNotExist
+                        messages.error(request, 'One of the items in your basket was not found \
+                        in our database. Please contact us for assistance.')
+                        order.delete()
+                        return redirect(reverse('view_bag'))
+
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse(
+                'checkout_success',
+                args=[order.order_number])
+                )
+
+        else:
+            messages.error(request, 'There was an error with your order form. \
+                Please double check your information.')
+
+    else:
+        bag = request.session.get('bag', {})
+        if not bag:
+            messages.error(request, 'Your basket is currently empty')
+            return redirect(reverse('product_list'))
+
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        stripe_total = round(total * 100)
+
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+            )   
+
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
+
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+
+        }
+        return render(request, 'checkout/checkout.html', context)
+
+
+def checkout_success(request, order_number):
+    """ Handles successful checkouts """
+    dishes = Dishes.objects.all()
+    wines = Wines.objects.all()
+    bundles = Bundle.objects.all()
+
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        order.user_profile = profile
+        order.save()
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+            }
+
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
+    messages.success(request, f'Your order ({order_number}) has been \
+        successfully processed. We have sent you a confirmation email \
+            to {order.email}.')
+
+    if 'bag' in request.session:
+        del request.session['bag']
+
+    context = {
+        'order': order,
+        'mugs':mugs,
+    }
+
+    return render(request, 'checkout/checkout-success.html', context)
+
+
+@require_POST
+def cache_checkout_data(request):
+    """
+    Determin if the user has the save-info box checked and add
+    to the metadata for the payment intent
+    """
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as exception:
+        messages.error(request, ('Sorry, your payment cannot be '
+                                 'processed right now. Please try '
+                                 'again later.'))
+        return HttpResponse(content=exception, status=400)
