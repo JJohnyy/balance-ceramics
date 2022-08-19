eb69f183d2e08e53f3021a102956586a22d89f5a
JJohnyy <jan.jasa@email.cz>
2022-08-19 08:42:11 +0000

    index.css

--- balance_ceramics/settings.py
+++ balance_ceramics/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-import dj_database_url
+# import dj_database_url
 
 if os.path.isfile('env.py'):
     import env
