76cc7c326c2bdf771d8ed98f4515732488b89892
JJohnyy <jan.jasa@email.cz>
2022-08-22 13:30:57 +0000

    bug fix in item detail html

--- bag/views.py
+++ bag/views.py
@@ -13,7 +13,7 @@ def view_bag(request):
 def add_to_bag(request, item_slug):
     """ Add a quantity of a product to the bag during a session """
 
-    quantity = int(request.POST.get('item-quantity'))
+    quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     product_name = request.POST.get('product_name')
     bag = request.session.get('bag', {})
