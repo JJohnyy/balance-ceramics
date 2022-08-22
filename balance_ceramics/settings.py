6f2b0cfce8146447700ea6b4d53a7bfe5733f2b3
JJohnyy <jan.jasa@email.cz>
2022-08-22 15:05:08 +0000

    updated secretkey

--- balance_ceramics/settings.py
+++ balance_ceramics/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qj1tcc@_x9=^2p6dy02x*p7=hfps&4brev7^jewu5f5&)woplw'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
