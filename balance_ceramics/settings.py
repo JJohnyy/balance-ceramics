7d4add67cf3cd1190a4414d0cb34474f5a301996
JJohnyy <jan.jasa@email.cz>
2022-08-22 15:47:46 +0000

    amazon swettings

--- balance_ceramics/settings.py
+++ balance_ceramics/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'profiles',
     'checkout',
     'crispy_forms',
-
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -118,7 +118,7 @@ WSGI_APPLICATION = 'balance_ceramics.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATBASE_URL = 'postgres://lmohqszbpmfpot:aedc3c15e67e05df4da2f7a27009662ec1d37b89d3fcc349468c33174eb45972@ec2-54-228-125-183.eu-west-1.compute.amazonaws.com:5432/d8550h4prlts9g'
+#DATBASE_URL = 'postgres://lmohqszbpmfpot:aedc3c15e67e05df4da2f7a27009662ec1d37b89d3fcc349468c33174eb45972@ec2-54-228-125-183.eu-west-1.compute.amazonaws.com:5432/d8550h4prlts9g'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
@@ -177,6 +177,23 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 
 
+if 'USE_AWS' in os.environ:
+    AWS_STORAGE_BUCKET_NAME = 'balance-ceramics'
+    AWS_S3_REGION_NAME = 'eu-central-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -198,5 +215,6 @@ else:
 
 
 # Stripe
+STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
