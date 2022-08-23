ac81222d480d42d6bf6004755a010fd373214204
JJohnyy <jan.jasa@email.cz>
2022-08-23 14:37:17 +0000

    changing item slug to item id in add to bag view

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from products.models import Mugs
 
@@ -10,17 +10,17 @@ def view_bag(request):
     return render(request, 'bag/bag.html')
 
 
-def add_to_bag(request, item_slug):
+def add_to_bag(request, item_id):
     """ Add a quantity of a product to the bag during a session """
 
-    mug = get_object_or_404(Mugs, pk=item_slug)
+    mug = get_object_or_404(Mugs, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     product_name = request.POST.get('product_name')
     bag = request.session.get('bag', {})
 
-    if item_slug in list(bag.keys()):
-        bag[item_slug] += quantity
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
         if quantity > 1:
             messages.success(
                 request,
@@ -36,13 +36,13 @@ def add_to_bag(request, item_slug):
             request.session['bag'] = bag
             return redirect(redirect_url)
     else:
-        bag[item_slug] = quantity
+        bag[item_id] = quantity
     
     request.session['bag'] = bag
     return redirect(redirect_url)
 
 
-def update_bag(request, item_slug):
+def update_bag(request, item_id):
     """ Update the quantity of a product to the bag during a session """
 
     if request.method == 'POST':
@@ -50,27 +50,27 @@ def update_bag(request, item_slug):
         product_name = request.POST.get('item_name')
         bag = request.session.get('bag', {})
 
-        if item_slug in list(bag.keys()):
+        if item_id in list(bag.keys()):
             if quantity > 0:
-                bag[item_slug] = quantity
+                bag[item_id] = quantity
                 messages.success(
                     request,
                     f'Quantity of {product_name.title()} successfully \
                         updated to {quantity}'
                     )
         else:
-            bag[item_slug] = quantity
+            bag[item_id] = quantity
 
         request.session['bag'] = bag
 
     return redirect(reverse('view_bag'))
 
 
-def remove_from_bag(request, item_slug):
+def remove_from_bag(request, item_id):
     """ Removes the item from the bag """
 
     bag = request.session.get('bag', {})
-    bag.pop(item_slug)
+    bag.pop(item_id)
     request.session['bag'] = bag
 
     messages.success(request, 'Successfully removed item from your basket')
