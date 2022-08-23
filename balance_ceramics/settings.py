adb20ac5eebdf5528ec3650c6b44406cc0c1adf6
JJohnyy <jan.jasa@email.cz>
2022-08-23 12:55:13 +0000

    updated checkout app with js and email

--- balance_ceramics/settings.py
+++ balance_ceramics/settings.py
@@ -180,6 +180,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
  # Bucket Config
 if 'USE_AWS' in os.environ:
+
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
+# Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'balance-ceramics'
     AWS_S3_REGION_NAME = 'eu-central-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
