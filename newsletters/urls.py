c5a1c38bd0e58414efbcfbfd22bc6a7c3e3af0cd
JJohnyy <jan.jasa@email.cz>
2022-08-25 11:17:39 +0000

    newsletters urls

--- newsletters/urls.py
+++ newsletters/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.newsletter_signup, name='newsletter_signup'),
-    path('newsletter_unsubscribe/', views.newsletter_unsubscribe, name='unsubscribe'),
-   
+    path('newsletter_unsubscribe/', views.newsletter_unsubscribe, name='news_unsubscribe'), 
 ]
