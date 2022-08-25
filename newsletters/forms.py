3dbabb01eb7b97b774e2f96d43cd99f59c7d5506
JJohnyy <jan.jasa@email.cz>
2022-08-25 17:24:38 +0000

    newsletter app, views css

--- newsletters/forms.py
+++ newsletters/forms.py
@@ -4,7 +4,13 @@ from .models import NewsletterUsers
 
 
 class NewsletterUserForm(forms.ModelForm):
+    helper = FormHelper()
+    helper.form_show_labels = False
     
     class Meta:
         model = NewsletterUsers
         fields = ['email']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        return email
