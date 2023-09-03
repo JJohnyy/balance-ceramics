a4b0bb4575f9566f6e5172545e6a400dd97349af
JJohnyy <jan.jasa@email.cz>
2023-09-03 04:33:45 +0000

    udated allowed hosts

--- balance_ceramics/settings.py
+++ balance_ceramics/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['balance-ceramics.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['balance-ceramics.herokuapp.com', 'localhost', 'https://seahorse-app-vn3bk.ondigitalocean.app/', 'seahorse-app-vn3bk.ondigitalocean.app', '*']
 
 
 # Application definition
