087ef4406dfbb0a7c3bd8ad1957601308db17f6b
JJohnyy <jan.jasa@email.cz>
2023-09-04 18:32:26 +0000

    changed secret key

--- balance_ceramics/settings.py
+++ balance_ceramics/settings.py
@@ -24,11 +24,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'k=c&_sk68k7j#k*)gh@)a0r-7q^p=23ub37*d3yoqj-z09bmcm)woplw')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['balance-ceramics.herokuapp.com', 'localhost', 'https://seahorse-app-vn3bk.ondigitalocean.app/', 'seahorse-app-vn3bk.ondigitalocean.app', '*']
 
